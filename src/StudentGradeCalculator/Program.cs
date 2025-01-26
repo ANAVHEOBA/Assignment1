88dad0e038d4c074c481f32673b140f95a526cd5
anavheoba <wisdomabraham92@gmail.com>
2025-01-26 08:50:49 -0500

    Initial project structure

---
+++ src/StudentGradeCalculator/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace StudentGradeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your program logic goes here
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
